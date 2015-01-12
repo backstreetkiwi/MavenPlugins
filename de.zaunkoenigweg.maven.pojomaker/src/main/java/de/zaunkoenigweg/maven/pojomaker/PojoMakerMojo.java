add5e4ee509e30c70d9607034c659b18e9fdae20
Nikolaus Winter <mail@nikolaus-winter.de>
2015-01-12 15:23:15 +0100

    Plugin erzeugt Java-Files und konfiguriert src-Folder

--- de.zaunkoenigweg.maven.pojomaker/src/main/java/de/zaunkoenigweg/maven/pojomaker/PojoMakerMojo.java
+++ de.zaunkoenigweg.maven.pojomaker/src/main/java/de/zaunkoenigweg/maven/pojomaker/PojoMakerMojo.java
@@ -1,7 +1,13 @@
 package de.zaunkoenigweg.maven.pojomaker;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -22,53 +28,67 @@ public class PojoMakerMojo extends AbstractMojo {
     private MavenProject mavenProject;
 
     @Parameter
-    // ( defaultValue = "${project.basedir}/src/main/resources/foo.txt")
-    private File fooFile;
+    private File pojoDefinitionFile;
 
     @Parameter
-    // ( defaultValue = "${project.build.directory}/foo")
-    private File fooClassFolder;
+    private File pojoClassesFolder;
 
+    @SuppressWarnings("unchecked")
     public void execute() throws MojoExecutionException, MojoFailureException {
 
-        getLog().info("PojoMaker not yet implemented.");
+        if (this.pojoDefinitionFile == null) {
+            throw new MojoExecutionException("Es wurde kein POJO-File (Attribut pojoDefinitionFile) angegeben.");
+        }
 
-        // try {
-        // File file = new File("/home/nikolaus/temp",
-        // String.format("Hugo%s.java", (new Date()).getTime()));
-        // PrintWriter printWriter = new PrintWriter(file);
-        // printWriter.println("test");
-        // printWriter.flush();
-        // printWriter.close();
-        // } catch (FileNotFoundException e) {
-        // // TODO Auto-generated catch block
-        // e.printStackTrace();
-        // }
+        if (!this.pojoDefinitionFile.exists()) {
+            throw new MojoExecutionException(String.format("Das POJO-File '%s' existiert nicht.", this.pojoDefinitionFile));
+        }
 
-        // getLog().info("I am the new Foo-Plugin");
-        // getLog().info(String.format("This is where I expect the Foo-definition to be found: %s",
-        // this.fooFile));
-        // getLog().info(String.format("This is where I put the classes: %s",
-        // this.fooClassFolder));
-        //
-        // mavenProject.addCompileSourceRoot(this.fooClassFolder.getAbsolutePath());
-        //
+        if (this.pojoClassesFolder == null) {
+            throw new MojoExecutionException("Es wurde kein Folder für die POJO-Klassen (Attribut pojoClassesFolder) angegeben.");
+        }
+
+        List<String> pojoDefinitionList = null;
+        try {
+            pojoDefinitionList = FileUtils.readLines(this.pojoDefinitionFile, "UTF-8");
+        } catch (IOException e) {
+            throw new MojoExecutionException(String.format("Das POJO-File '%s' konnte nicht gelesen werden.", this.pojoDefinitionFile), e);
+        }
+
+        if (pojoDefinitionList.isEmpty()) {
+            throw new MojoExecutionException(String.format("Das POJO-File '%s' enthält keine Einträge.", this.pojoDefinitionFile));
+        }
+
+        getLog().info(String.format("Das POJO-File '%s' enthält %d Einträge.", this.pojoDefinitionFile, pojoDefinitionList.size()));
+
+        this.pojoClassesFolder.mkdirs();
+        try {
+            FileUtils.cleanDirectory(this.pojoClassesFolder);
+        } catch (IOException e) {
+            throw new MojoExecutionException(String.format("Das POJO-Class-Verzeichnis '%s' konnte nicht geleert werden.", this.pojoClassesFolder), e);
+        }
+        for (String pojoName : pojoDefinitionList) {
+            createPojoClass(pojoName);
+        }
+
+        mavenProject.addCompileSourceRoot(this.pojoClassesFolder.getAbsolutePath());
+
+        // TODO nach delta gucken
         // if (this.plexusBuildContext.hasDelta(fooFile)) {
-        // try {
-        // // FileUtils.cleanDirectory(fooClassFolder);
-        // OutputStream fileOutputStream =
-        // plexusBuildContext.newFileOutputStream(new File(this.fooClassFolder,
-        // String.format("Hugo%s.java", (new Date()).getTime())));
-        // PrintWriter printWriter = new PrintWriter(fileOutputStream);
-        // printWriter.println("Hallo");
-        // printWriter.flush();
-        // fileOutputStream.close();
-        // } catch (IOException e) {
-        // // TODO Auto-generated catch block
-        // e.printStackTrace();
-        // }
-        // }
-
     }
 
+    private void createPojoClass(String name) {
+        try {
+            OutputStream fileOutputStream = plexusBuildContext.newFileOutputStream(new File(this.pojoClassesFolder, String.format("%s.java", name)));
+            PrintWriter printWriter = new PrintWriter(fileOutputStream);
+            printWriter.println(String.format("public class %s {", name));
+            printWriter.println("}");
+            printWriter.println("//" + new Date());
+            printWriter.flush();
+            fileOutputStream.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 }
