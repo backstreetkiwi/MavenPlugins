9cb7e6111a82387e31ffaa042ec264a9633a1271
Nikolaus Winter <mail@nikolaus-winter.de>
2015-01-12 12:12:34 +0100

    Mojo an Phase generate-sources gebunden.

--- de.zaunkoenigweg.maven.pojomaker/src/main/java/de/zaunkoenigweg/maven/pojomaker/PojoMakerMojo.java
+++ de.zaunkoenigweg.maven.pojomaker/src/main/java/de/zaunkoenigweg/maven/pojomaker/PojoMakerMojo.java
@@ -6,12 +6,13 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 import org.sonatype.plexus.build.incremental.BuildContext;
 
-@Mojo(name = "generatePojoClasses")
+@Mojo(name = "generatePojoClasses", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 public class PojoMakerMojo extends AbstractMojo {
 
     @Component
